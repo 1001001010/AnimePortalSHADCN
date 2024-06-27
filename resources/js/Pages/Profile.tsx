cb53c9df6f27b2ccf186c4a66064569458140271
1001001010 <lll100100@yandex.ru>
2024-06-27 22:21:27 +0800

    fix

--- resources/js/Pages/Profile.tsx
+++ resources/js/Pages/Profile.tsx
@@ -1,17 +1,13 @@
 import { Link, Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
 import Header from "@/Components/Header";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/shadcn/ui/card";
-import { Input } from "@/shadcn/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/shadcn/ui/popover";
+import { CalendarIcon } from "@radix-ui/react-icons";
 import { Button } from "@/shadcn/ui/button";
-import { Checkbox } from "@/shadcn/ui/checkbox";
+import { Calendar } from "@/shadcn/ui/calendar";
+import * as React from "react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
 
 export default function Profile({
     auth,
@@ -26,10 +22,49 @@ export default function Profile({
             ?.classList.add("!flex-row");
         document.getElementById("background")?.classList.add("!hidden");
     };
-
+    export function DatePickerDemo() {
+        const [date, setDate] = React.useState<Date>()
     return (
         <>
             <Header auth={auth} />
+
+            <div className="ml-14">
+                <div className="m-4 border border-gray-200 rounded-lg shadow dark:border-gray-700">
+                    <div className="p-6 text-gray-900 dark:text-gray-100 grid grid-cols-4 gap-4">
+                        <div>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant={"outline"}
+                                        className={cn(
+                                            "w-[240px] justify-start text-left font-normal",
+                                            !date && "text-muted-foreground"
+                                        )}
+                                    >
+                                        <CalendarIcon className="mr-2 h-4 w-4" />
+                                        {date ? (
+                                            format(date, "PPP")
+                                        ) : (
+                                            <span>Pick a date</span>
+                                        )}
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent
+                                    className="w-auto p-0"
+                                    align="start"
+                                >
+                                    <Calendar
+                                        mode="single"
+                                        selected={date}
+                                        onSelect={setDate}
+                                        initialFocus
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
