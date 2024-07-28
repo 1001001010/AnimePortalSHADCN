6c53c3568070ccc0371947a61d6109404edeb3b7
1001001010 <lll100100@yandex.ru>
2024-07-28 23:45:09 +0800

    Сохранение нового аниме

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Anime;
 
 class AdminController extends Controller
 {
@@ -14,8 +15,8 @@ class AdminController extends Controller
     public function new_anime(Request $request) {
 
         $validated = $request->validate([
-            'age' => 'required|integer',
-            'status' => 'required|string',
+            'age' => 'required|in:0,6,12,16,18',
+            'status' => 'required|in:ongoing,came_out,preview',
             'name' => 'required|string',
             'type' => 'required|string',
             'original' => 'required|string',
@@ -29,6 +30,35 @@ class AdminController extends Controller
             'screens.*' => 'required|image|mimes:jpeg,png,jpg|max:2048',
         ]);
 
-        return dd($request->all());
+        // Работа с обложкой
+        $cover_name = time(). "." . $request->cover->extension();
+        $cover_path = $request->cover->storeAs('public/covers', $cover_name);
+
+        $screens = [];
+        foreach ($request->screens as $screen) {
+            $screen_name = time() . "_" . uniqid() . "." . $screen->extension();
+            $screen_path = $screen->storeAs('public/screens', $screen_name);
+            $screens[] = $screen_path;
+        }
+        $screens_json = json_encode($screens);
+
+        // Создание новой записи в таблице animes
+        $anime = new Anime();
+        $anime->age = $validated['age'];
+        $anime->status = $validated['status'];
+        $anime->name = $validated['name'];
+        $anime->type = $validated['type'];
+        $anime->original = $validated['original'];
+        $anime->studio = $validated['studio'];
+        $anime->voice = $validated['voice'];
+        $anime->director = $validated['director'];
+        $anime->autor = $validated['autor'];
+        $anime->description = $validated['description'];
+        $anime->grade = 0;
+        $anime->cover = $cover_path;
+        $anime->screens = $screens_json;
+        $anime->save();
+
+        return redirect()->back()->with('success', 'Аниме успешно добавлено!');
     }
 }
---
+++ database/migrations/2024_07_28_121947_create_animes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->enum('age', ['0', '6', '12', '16', '18']);
+            $table->enum('status', ['ongoing', 'came_out', 'preview']);
+            $table->string('type');
+            $table->string('name');
+            $table->string('original');
+            $table->string('studio');
+            $table->string('voice');
+            $table->string('director');
+            $table->string('autor');
+            $table->text('description');
+            $table->string('cover');
+            $table->json('screens');
+            $table->float('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animes');
+    }
+};
