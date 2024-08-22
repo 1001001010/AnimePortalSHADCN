a4a6c05d849bf7277642342f9431877df12c1e66
1001001010 <lll100100@yandex.ru>
2024-08-22 11:10:34 +0800

    небольшой фикс страницы Friends

--- resources/js/Components/Friends/SearchFriends.tsx
+++ resources/js/Components/Friends/SearchFriends.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { FormEventHandler } from "react";
+import React, { FormEventHandler, useState } from "react";
 import { useForm } from "@inertiajs/react";
 import { FriendShips, PageProps } from "@/types";
 import {
@@ -21,8 +19,9 @@ export default function SearchFriends({
     users: any[];
     request: FriendShips[];
 }>) {
-    const [open, setOpen] = React.useState(false);
-    const [value, setValue] = React.useState("");
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState("");
+    const [focused, setFocused] = useState(false); // Add a new state for focus
     const { data, setData, post, processing, errors } = useForm({
         friend_id: null,
     });
@@ -40,58 +39,67 @@ export default function SearchFriends({
                     <CommandInput
                         placeholder="Введите имя"
                         className="min-w-full"
+                        onFocus={() => setFocused(true)}
+                        onBlur={() => setFocused(false)}
+                        onClick={(e) =>
+                            setValue((e.target as HTMLInputElement).value)
+                        }
                     />
-                    <CommandList>
-                        <CommandEmpty>Пользователь не найден</CommandEmpty>
-                        <CommandGroup>
-                            <form onSubmit={submit}>
-                                {users.map((user, index) => (
-                                    <button
-                                        type="submit"
-                                        className="w-full"
-                                        key={index}
-                                    >
-                                        <CommandItem
-                                            value={user.name}
-                                            onSelect={(currentValue) => {
-                                                setValue(
-                                                    currentValue === value
-                                                        ? ""
-                                                        : currentValue
-                                                );
-                                                setOpen(false);
-                                                setData({
-                                                    friend_id: user.id,
-                                                });
-                                            }}
+                    {focused || value ? ( // Render CommandList only when focused or has value
+                        <CommandList>
+                            <CommandEmpty>Пользователь не найден</CommandEmpty>
+                            <CommandGroup>
+                                <form onSubmit={submit}>
+                                    {users.map((user, index) => (
+                                        <button
+                                            type="submit"
+                                            className="w-full"
+                                            key={index}
                                         >
-                                            <Check
-                                                className={cn(
-                                                    "mr-2 h-4 w-4",
-                                                    value === user.id
-                                                        ? "opacity-100"
-                                                        : "opacity-0"
+                                            <CommandItem
+                                                value={user.name}
+                                                onSelect={(currentValue) => {
+                                                    setValue(
+                                                        currentValue === value
+                                                            ? ""
+                                                            : currentValue
+                                                    );
+                                                    setOpen(false);
+                                                    setData({
+                                                        friend_id: user.id,
+                                                    });
+                                                }}
+                                            >
+                                                <Check
+                                                    className={cn(
+                                                        "mr-2 h-4 w-4",
+                                                        value === user.id
+                                                            ? "opacity-100"
+                                                            : "opacity-0"
+                                                    )}
+                                                />
+                                                {request.some(
+                                                    (req) =>
+                                                        req.friend_id ===
+                                                        user.id
+                                                ) ? (
+                                                    <div className="flex flex-wrap flex-row justify-between w-full">
+                                                        <p>{user.name}</p>
+                                                        <p className="text-gray-500">
+                                                            Заявка уже
+                                                            отправлена
+                                                        </p>
+                                                    </div>
+                                                ) : (
+                                                    user.name
                                                 )}
-                                            />
-                                            {request.some(
-                                                (req) =>
-                                                    req.friend_id === user.id
-                                            ) ? (
-                                                <div className="flex flex-wrap flex-row justify-between w-full">
-                                                    <p>{user.name}</p>
-                                                    <p className="text-gray-500">
-                                                        Заявка уже отправлена
-                                                    </p>
-                                                </div>
-                                            ) : (
-                                                user.name
-                                            )}
-                                        </CommandItem>
-                                    </button>
-                                ))}
-                            </form>
-                        </CommandGroup>
-                    </CommandList>
+                                            </CommandItem>
+                                        </button>
+                                    ))}
+                                </form>
+                            </CommandGroup>
+                        </CommandList>
+                    ) : null}
                 </Command>
             </ul>
         </>
