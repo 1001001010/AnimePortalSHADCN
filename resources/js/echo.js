c1ef82cea15a58985048dc93c2ecd009e1ac85d8
1001001010 <lll100100@yandex.ru>
2024-09-03 11:18:44 +0800

    подключил pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,13 +1,6 @@
 import Echo from "laravel-echo";
+
 import Pusher from "pusher-js";
-
-declare global {
-    interface Window {
-        Pusher: typeof Pusher;
-        Echo: Echo;
-    }
-}
-
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
