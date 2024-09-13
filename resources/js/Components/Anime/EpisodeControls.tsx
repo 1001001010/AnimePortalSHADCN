d280aa4c4b58b32eed829182155058bd9abc7381
1001001010 <lll100100@yandex.ru>
2024-09-13 12:18:11 +0800

    небольшой фикс стилей кнопок навигации под плеером

--- resources/js/Components/Anime/EpisodeControls.tsx
+++ resources/js/Components/Anime/EpisodeControls.tsx
@@ -14,50 +14,46 @@ export default function EpisodeControls({
     previousEpisode?: Episode;
 }>) {
     return (
-        <>
-            <div className="flex flex-row justify-between pt-4">
-                {previousEpisode &&
-                previousEpisode.seasonNumber &&
-                previousEpisode.episode &&
-                previousEpisode.episode.number ? (
-                    <Link
-                        href={route("anime", [
-                            Anime.unix,
-                            previousEpisode?.seasonNumber,
-                            previousEpisode?.episode?.number,
-                        ])}
-                        preserveScroll
-                    >
-                        <Button>Предыдущая серия</Button>
-                    </Link>
-                ) : (
-                    <Button variant={"ghost"} className="invisible">
-                        Предыдущая серия
-                    </Button>
-                )}
-                <Link href={route("anime", [Anime.unix])} preserveScroll>
-                    <Button variant="outline">Список всех серий</Button>
+        <div className="flex pt-4">
+            {previousEpisode &&
+            previousEpisode.seasonNumber &&
+            previousEpisode.episode &&
+            previousEpisode.episode.number ? (
+                <Link
+                    href={route("anime", [
+                        Anime.unix,
+                        previousEpisode?.seasonNumber,
+                        previousEpisode?.episode?.number,
+                    ])}
+                    preserveScroll
+                    className="mr-auto"
+                >
+                    <Button>Предыдущая серия</Button>
                 </Link>
-                {nextEpisode &&
-                nextEpisode.seasonNumber &&
-                nextEpisode.episode &&
-                nextEpisode.episode.number ? (
-                    <Link
-                        href={route("anime", [
-                            Anime.unix,
-                            nextEpisode?.seasonNumber,
-                            nextEpisode?.episode?.number,
-                        ])}
-                        preserveScroll
-                    >
-                        <Button>Следующая серия</Button>
-                    </Link>
-                ) : (
-                    <Button variant={"ghost"} className="invisible">
-                        Следующая серия
-                    </Button>
-                )}
-            </div>
-        </>
+            ) : (
+                <div className="mr-auto" />
+            )}
+            <Link href={route("anime", [Anime.unix])} preserveScroll>
+                <Button variant="outline">Список всех серий</Button>
+            </Link>
+            {nextEpisode &&
+            nextEpisode.seasonNumber &&
+            nextEpisode.episode &&
+            nextEpisode.episode.number ? (
+                <Link
+                    href={route("anime", [
+                        Anime.unix,
+                        nextEpisode?.seasonNumber,
+                        nextEpisode?.episode?.number,
+                    ])}
+                    preserveScroll
+                    className="ml-auto"
+                >
+                    <Button>Следующая серия</Button>
+                </Link>
+            ) : (
+                <div className="ml-auto" />
+            )}
+        </div>
     );
 }
