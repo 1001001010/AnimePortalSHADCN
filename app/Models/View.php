ad5712d1db9892d056ff70567181a0416493d17f
1001001010 <lll100100@yandex.ru>
2024-10-18 12:44:00 +0800

    Добавление новых файлов

---
+++ app/Models/View.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class View extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['anime_id', 'user_id', 'ip_address'];
+
+    public function anime()
+    {
+        return $this->belongsTo(Anime::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
